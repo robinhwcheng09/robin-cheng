8241e70b7d6d3d9abf08b34b987a826c7ea20d94
robincheng <robincheng09@gmail.com>
2025-12-18 13:41:58 +0800

    add github actions for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://robinhwcheng09.github.io',
+    base: '/robin-cheng',
 });
